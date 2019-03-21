41df96af4e9f8144f9258aaf3725faf2e7567aee
Rob Brennan <rob@therobbrennan.com>
2019-03-21 15:55:24 -0700

    Added resolvers for Mission, Launch, and User types

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -40,4 +40,31 @@ module.exports = {
     me: async (_, __, { dataSources }) =>
       dataSources.userAPI.findOrCreateUser(),
   },
+  Mission: {
+    // Make sure the default size is 'large' in case the user doesn't specify
+    // Note how the first argument passed into the resolve is the parent, which is the mission object.
+    missionPatch: (mission, { size } = { size: 'LARGE' }) => {
+      return size === 'SMALL'
+        ? mission.missionPatchSmall
+        : mission.missionPatchLarge
+    },
+  },
+  Launch: {
+    isBooked: async(launch, _, { dataSources }) => dataSources.userAPI.isBookedOnLaunch({ launchId: launch.id }),
+  },
+  User: {
+    trips: async(_, __, { dataSources }) => {
+      // Get ids of launches by user
+      const launchIds = await dataSources.userAPI.getLaunchIdsByUser()
+
+      if (!launchIds.length) return []
+
+      // Look up those launches by their ids
+      return (
+        dataSources.launchAPI.getLaunchesByIds({
+          launchIds,
+        }) || []
+      )
+    },
+  },
 }
