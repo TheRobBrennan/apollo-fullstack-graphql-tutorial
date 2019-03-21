9eb0f612b505178e952acbba700f23969558122a
Rob Brennan <rob@therobbrennan.com>
2019-03-21 16:03:38 -0700

    Added context function for lightweight user authentication

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -8,7 +8,30 @@ const LaunchAPI = require('./datasources/launch')
 const UserAPI = require('./datasources/user')
 const store = createStore()
 
+// Email validation
+const isEmail = require('isemail')
+
 const server = new ApolloServer({
+  // The context function on your ApolloServer instance is called with the request object each time a GraphQL operation hits your API. Use this request object to read the authorization headers.
+  context: async({ req }) => {
+    // Authenticate the user within the context function.
+    //
+    // While we definitely don’t advocate using this specific implementation in production since it’s not secure, all of the concepts outlined here are transferable to how you’ll implement authentication in a real world application.
+
+    // Simple auth check on every GraphQL request
+    const auth = (req.headers && req.headers.authorization) || ''
+    const email = Buffer.from(auth, 'base64').toString('ascii')
+
+    // If the email isn't formatted validly, return null for the user
+    if (!isEmail.validate(email)) return { user: null }
+
+    // Find a user by their email
+    const users = await store.users.findOrCreate({ where: { email }})
+    const user = users && users[0] ? users[0] : null
+
+    // Once the user is authenticated, attach the user to the object returned from the context function.
+    return { user: { ...user.dataValues }}
+  },
   typeDefs,
   resolvers,
   dataSources: () => ({
