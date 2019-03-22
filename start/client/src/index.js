a7438689a97afa0305abbb1947d213aa0f9ea9b3
Rob Brennan <rob@therobbrennan.com>
2019-03-21 19:29:12 -0700

    Adding authorization headers to every GraphQL operation

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -9,13 +9,26 @@ import { HttpLink } from 'apollo-link-http'
 
 const cache = new InMemoryCache()
 const link = new HttpLink({
-  uri: `https://fullstack-tutorial-server-wvigdpddtw.now.sh`
+  uri: 'http://localhost:4000/graphql',
+  headers: {
+    // This token is read every time a GraphQL operation is made
+    authorization: localStorage.getItem('token'),
+  },
 })
 
 // Define our Apollo client
 const client = new ApolloClient({
   cache,
   link
+  // resolvers,
+  // typeDefs,
+})
+
+cache.writeData({
+  data: {
+    isLoggedIn: !!localStorage.getItem('token'),
+    cartItems: [],
+  }
 })
 
 // Wrap our app in the Apollo provider
