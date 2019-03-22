6778c4dfb03c361075d6043c7527953fed95a0c3
Rob Brennan <rob@therobbrennan.com>
2019-03-21 17:47:04 -0700

    Added ability to incorporate pagination and load more launches

--- start/client/src/pages/launches.js
+++ start/client/src/pages/launches.js
@@ -31,7 +31,7 @@ const GET_LAUNCHES = gql`
 export default function Launches() {
   return (
     <Query query={GET_LAUNCHES}>
-      {({ data, loading, error }) => {
+      {({ data, loading, error, fetchMore }) => {
         if (loading) return <Loading />
         if (error) return <p>ERROR</p>
 
@@ -43,6 +43,33 @@ export default function Launches() {
               data.launches.launches.map(launch => (
                 <LaunchTile key={launch.id} launch={launch} />
               ))}
+
+            {/* Load more */}
+            {data.launches && data.launches.hasMore && (
+              <Button onClick={() => {
+                fetchMore({
+                  variables: {
+                    after: data.launches.cursor,
+                  },
+                  updateQuery: (prev, { fetchMoreResult, ...rest}) => {
+                    if (!fetchMoreResult) return prev
+
+                    return {
+                      ...fetchMoreResult,
+                      launches: {
+                        ...fetchMoreResult.launches,
+                        launches: [
+                          ...prev.launches.launches,
+                          ...fetchMoreResult.launches.launches,
+                        ],
+                      },
+                    }
+                  },
+                })
+              }}>
+                Load More
+              </Button>
+            )}
           </Fragment>
         )
       }}
